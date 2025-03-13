8037884b79ed7f2ff75b7d2c66b00ad71fdca47a
Pierre Nouhen <pierre.nouhen@orange.fr>
2025-03-13 16:22:35 +0100

    Avancement sur le scss du slideShow

--- src/components/ui/SlideShow.jsx
+++ src/components/ui/SlideShow.jsx
@@ -23,42 +23,40 @@ export default function SlideShows() {
         <>
           <div className="reviews___container">
             <i
-              class="fa-solid fa-chevron-left"
+              className="fa-solid fa-chevron-left"
               onClick={() =>
                 setCurrentIndex(
                   (index) => (index - 1 + reviews.length) % reviews.length
                 )
               }
             ></i>
-            <div className="slideShow">
-              <p className="name">{reviews[currentIndex].name}</p>
-              <div className="ratings">
-                {[1, 2, 3, 4, 5].map((starIndex) => (
-                  <i
-                    key={starIndex}
-                    className={`fa-solid fa-star ${
-                      starIndex <= reviews.rating ? "active" : ""
-                    }`}
-                  ></i>
-                ))}
-              </div>
-              <p className="comment">{reviews[currentIndex].comment}</p>
+            <p className="name">{reviews[currentIndex].name}</p>
+            <div className="ratings">
+              {[1, 2, 3, 4, 5].map((starIndex) => (
+                <i
+                  key={starIndex}
+                  className={`fa-solid fa-star ${
+                    starIndex <= reviews[currentIndex].rating ? "active" : ""
+                  }`}
+                ></i>
+              ))}
             </div>
+            <p className="comment">{reviews[currentIndex].comment}</p>
             <i
-              class="fa-solid fa-chevron-right"
+              className="fa-solid fa-chevron-right"
               onClick={() =>
                 setCurrentIndex((index) => (index + 1) % reviews.length)
               }
             ></i>
+            <ul>
+              {reviews.map((item, index) => (
+                <li
+                  key={index}
+                  className={`${currentIndex === index ? "dotSelected" : ""}`}
+                ></li>
+              ))}
+            </ul>
           </div>
-          <ul>
-            {reviews.map((item, index) => (
-              <li
-                key={index}
-                className={`dot ${currentIndex === index ? "dotSelected" : ""}`}
-              ></li>
-            ))}
-          </ul>
         </>
       ) : (
         <div>Chargement...</div>
